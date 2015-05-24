6b61760ad6686544083f84ed8cf0de18d1f532cf
wirsing <wirsingwirsing@googlemail.com>
2015-05-24 15:34:54 -0700

    fixed square oscillator bug

--- src/processing/sound/SqrOsc.java
+++ src/processing/sound/SqrOsc.java
@@ -67,7 +67,7 @@ public class SqrOsc implements SoundObject {
 	
 	private void set(){
 		if(m_nodeId[0] != -1 ) {
-			m_engine.sqrSet(m_freq, m_amp, m_add, m_pos, m_nodeId);
+			m_engine.sqrSet(m_freq, m_amp, m_add-1, m_pos, m_nodeId);
 		}	
 	}
 	
